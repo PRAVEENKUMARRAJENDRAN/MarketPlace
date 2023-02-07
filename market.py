1417bb60d6836365948367e326415ab4c75f1501
PRAVEENKUMARRAJENDRAN <praveenrajendran2109@gmail.com>
2023-02-06 19:32:50 -0500

    Template Inheritance Data sending

--- market.py
+++ market.py
@@ -6,9 +6,17 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/home')
-def hello_world():
+def home_page():
     return render_template('home.html')
 
 @app.route('/about/<username>')
 def about(username):
     return f'<h1>{username} king</h1>'  
+
+@app.route('/market')
+def market_page():
+    item_list = [
+        { 'id':1,'name':'Phone','barcode':'||2349068','price':'500'},
+        { 'id':2,'name':'Laptop','barcode':'||2349456','price':'500'},
+    ]
+    return render_template('market.html',items = item_list)
